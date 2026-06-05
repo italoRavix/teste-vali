6f089631ed04b2ba54994817837a9be65412a981
hendryl-figueiredomiranda-prog <hendryl-figueiredo.miranda@ufvjm.edu.br>
2026-06-05 01:20:02 -0300

    Adicionando botões dos filtros e logica visual

--- js/home.js
+++ js/home.js
@@ -1,13 +1,31 @@
-const menuOpenButton = document.querySelector('#menu-open-button');
-const menuCloseButton = document.querySelector('#menu-close-button');
+const botaoAbrirMenu = document.querySelector("#botao-abrir-menu");
+const botaoFecharMenu = document.querySelector("#botao-fechar-menu");
 
+if (botaoAbrirMenu && botaoFecharMenu) {
+    botaoAbrirMenu.addEventListener("click", () => {
+        document.body.classList.add("show-mobile-menu");
+    });
 
-menuOpenButton.addEventListener('click', () => {
-    //abir menu mobile
-    document.body.classList.toggle("show-mobile-menu");
+    botaoFecharMenu.addEventListener("click", () => {
+        document.body.classList.remove("show-mobile-menu");
+    });
+}
+
+document.querySelectorAll(".filtro-btn").forEach(botao => {
+    botao.addEventListener("click", () => {
+
+        // Se já estiver ativo, desativa
+        if (botao.classList.contains("ativo")) {
+            botao.classList.remove("ativo");
+            return;
+        }
+
+        // Remove o ativo dos outros
+        document.querySelectorAll(".filtro-btn").forEach(btn => {
+            btn.classList.remove("ativo");
+        });
+
+        // Ativa o clicado
+        botao.classList.add("ativo");
+    });
 });
-
-
-    //fechar menu mobile
-menuCloseButton.addEventListener('click', () => menuOpenButton.click());
-
